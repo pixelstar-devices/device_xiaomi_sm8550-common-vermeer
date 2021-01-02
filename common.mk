9062c5594ba871ac10ad60480eec0f197d7f7ede
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-03 03:08:12 +0530

    sm8550-common: Show "Turbo charging" instead of "Charging rapidly"
    Like MIUI

--- common.mk
+++ common.mk
@@ -448,7 +448,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay
 
 # Parts
 PRODUCT_PACKAGES += \
