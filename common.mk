00345ca78fcff1888a848315c4f69d71f68a49d8
ralf979 <ralf979@gmail.com>
2024-10-04 09:29:00 +0200

    sm8550-common: Build common QTI vibrator HAL

--- common.mk
+++ common.mk
@@ -611,11 +611,9 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator-V1-ndk_platform.vendor \
-    vendor.qti.hardware.vibrator.service.xiaomi
-
+    vendor.qti.hardware.vibrator.service
 PRODUCT_COPY_FILES += \
-    hardware/xiaomi/aidl/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_COPY_FILES += \
