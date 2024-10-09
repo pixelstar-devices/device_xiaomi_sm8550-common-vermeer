156932de776d537162cc8c7e7fec966926a3dd76
SebaUbuntu <seba@sebaubuntu.dev>
2024-10-09 14:14:44 +0200

    sm8550-common: Build common Xiaomi touch HIDL

--- common.mk
+++ common.mk
@@ -578,6 +578,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi
+
 # Trusted User Interface
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.systemhelper@1.0.vendor
